38c8c0fa038379694984561c874bb0c39816b470
fowrr <smand3539@gmail.com>
2025-04-18 17:16:53 -0400

    thomas og version

---
+++ Intro-Game-1e9c177bcb553fc30b6511ddc7ed15dbc6bb38b1/Time Label.gd
@@ -0,0 +1,18 @@
+extends Label
+
+var timeLeft = 5
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	timeLeft = timeLeft - 1
+	text = "Time: " + str(timeLeft)
+	if timeLeft == 0:
+		
